3976c7ea4f5657407190eb2ad589f469fcec0a9c
DharaZatun <dhara@zatun.in>
2023-09-16 00:44:33 +0530

    player Input with keyboard

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : StateMachine
+{
+
+    public GameObject objPlayer;         // player's reference
+    //public Collider collider;           //player's colider
+                                        // public Rigidbody rigidbody;           //player's rigidbody
+   // public Transform transformCamera;   //followed camera reference
+    public float MovementSpeed { get; private set; } = 1f;
+    public float JumpForce { get; private set; } = 5f;
+    public float LookRotationDampFactor { get; private set; } = 5f;
+    public InputReader InputReader { get; set; }    //input system refeence
+
+    [HideInInspector]
+    public Vector3 PlayerLookDirection = Vector3.forward;   //current direction of player
+
+    private void Start()
+    {
+        InputReader = GetComponent<InputReader>();
+        SwitchState(new PlayerMoveState(this));
+
+    }
+}
